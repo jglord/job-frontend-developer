0217221257ddb6aede337b497a00861752d1d438
jgoulart <joao123goulart@gmail.com>
2021-04-30 13:52:34 -0300

    init cardVideos

---
+++ src/components/CardVideos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nes-container cardVideos is-centered">
+    <h1>CardVideos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardVideos",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+.cardVideos{
+    position:fixed;
+    left: 20%;
+    top: 20%;
+      height: 600px;
+  width: 1175px;
+}
+
+</style>
